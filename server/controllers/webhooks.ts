7555014b741db6a54138d66cfd911b2b98be48a1
andawa01 <evansandawa02@gmail.com>
2026-05-08 12:42:38 +0300

    update webhooks.ts file

--- server/controllers/webhooks.ts
+++ server/controllers/webhooks.ts
@@ -1,13 +1,12 @@
 import { verifyWebhook } from "@clerk/express/webhooks"
 import { Request, Response } from "express"
-import { env } from "node:process"
 import User from "../models/User.js"
 
 export const clerkWebhook = async (req: Request, res: Response) => {
   try {
     const evt = await verifyWebhook(req)
 
-    if (env.type === 'user.created' || env.type === 'user.updated') {
+    if (evt.type === 'user.created' || evt.type === 'user.updated') {
         const user = await User.findOne({clerkId: evt.data.id})
 
         const userData = {
